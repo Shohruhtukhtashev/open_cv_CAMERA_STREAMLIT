667608d810b757f7e428f0aa96c2c305ad5cb6a1
Shohruh Tukhtashev <87192481+Shohruhtukhtashev@users.noreply.github.com>
2023-08-02 17:19:09 +0500

    Update Stream_CV2_Video.py

--- Stream_CV2_Video.py
+++ Stream_CV2_Video.py
@@ -14,7 +14,7 @@ camera = cv2.VideoCapture(0)
 
 while run:
     _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     FRAME_WINDOW.image(frame)
 else:
     st.write('Stopped')
